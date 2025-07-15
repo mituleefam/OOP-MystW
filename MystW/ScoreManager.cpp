7e57626bcb50cf7909e65b61131511b2a30c5e0f
LAPTOP-889HRLJG\daiid <daiidinhho26@gmail.com>
2025-07-15 21:39:58 +0700

    add score manager

---
+++ MystW/ScoreManager.cpp
@@ -0,0 +1,52 @@
+#include "ScoreManager.hpp"
+#include <fstream>
+#include <algorithm>
+#include <iostream>
+const std::string ScoreManager::fileName = "scores.txt";
+std::vector<int> ScoreManager::scores;
+void ScoreManager::loadScore()
+{
+	scores.clear();
+	std::ifstream input(fileName);
+	if (!input.is_open())
+	{
+		std::cout << "Cannot open file";
+		return;
+	}
+	int score;
+	while (input >> score)
+		scores.push_back(score);
+	std::sort(scores.begin(), scores.end(), std::greater<>());
+	if (scores.size() >= 3)
+		scores.resize(3);
+	input.close();
+}
+
+void ScoreManager::addScore(int score)
+{
+	loadScore();
+	scores.push_back(score);
+	std::sort(scores.begin(), scores.end(), std::greater<>());
+	if (scores.size() >= 3)
+		scores.resize(3);
+	saveScore();
+}
+
+void ScoreManager::saveScore()
+{
+	std::ofstream out(fileName);
+	if (!out.is_open())
+	{
+		std::cout << "Cannot open file";
+		return;
+	}
+	for (int score : scores)
+		out << score << std::endl;
+	out.close();
+}
+
+std::vector<int> ScoreManager::getTopScore()
+{
+	loadScore();
+	return scores;
+}
---
+++ MystW/ScoreManager.hpp
@@ -0,0 +1,16 @@
+#pragma once
+#include <vector>
+#include <string>
+
+class ScoreManager
+{
+private:
+	static std::vector<int> scores;
+	static const std::string fileName ;
+	ScoreManager();
+public:
+	static void loadScore();
+	static void addScore(int score);
+	static std::vector<int> getTopScore();
+	static void saveScore();
+};
